542a5596595e3dfbaef0134c4ceaa9d424c1f37b
Mauricio Bedoya <bedoyama@gmail.com>
2026-07-03 10:01:35 -0500

    misc changes to code - minor

--- java-8/src/com/learnJava/functionalInterfaces/FunctionStudentExample.java
+++ java-8/src/com/learnJava/functionalInterfaces/FunctionStudentExample.java
@@ -7,16 +7,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FunctionStudentExample {
 
-    static Function<List<Student>, Map<String, Double>>  function = (students -> {
+    static Function<List<Student>, Map<String, Double>> getGoodStudents = (students -> {
 
-        Map<String,Double> studentGradeMap = new HashMap<>();
+        Map<String, Double> studentGradeMap = new HashMap<>();
         students.forEach((student -> {
 
-            if(PredicateStudentExample.gradeLevelIsGood.test(student)){
-                studentGradeMap.put(student.getName(),student.getGpa());
+            if (PredicateStudentExample.gpaIsGood.test(student)) {
+                studentGradeMap.put(student.getName(), student.getGpa());
             }
         }));
 
@@ -24,9 +25,33 @@ public class FunctionStudentExample {
 
     });
 
+    static Function<List<Student>, Map<String, Double>> getAllStudents = (students -> {
+
+        Map<String, Double> studentGradeMap = new HashMap<>();
+        students.forEach((student -> studentGradeMap.put(student.getName(), student.getGpa())));
+
+        return studentGradeMap;
+
+    });
+
+    // Later in the course
+    static Function<List<Student>, Map<String, Integer>> getGoodStudents2 =
+            students -> students.stream()
+                    .filter(PredicateStudentExample.gradeLevelIsGood)
+                    .collect(Collectors.toMap(Student::getName, Student::getGradeLevel));
+
+    static Function<List<Student>, Map<String, Integer>> getAllStudents2 =
+            students -> students.stream()
+                    .collect(Collectors.toMap(Student::getName, Student::getGradeLevel));
+
     public static void main(String[] args) {
 
-        System.out.println(function.apply(StudentDataBase.getAllStudents()));
+        System.out.println(getAllStudents.apply(StudentDataBase.getAllStudents()));
+        System.out.println(getGoodStudents.apply(StudentDataBase.getAllStudents()));
+
+        System.out.println("------------------------------------");
+        System.out.println(getAllStudents2.apply(StudentDataBase.getAllStudents()));
+        System.out.println(getGoodStudents2.apply(StudentDataBase.getAllStudents()));
 
     }
 }
