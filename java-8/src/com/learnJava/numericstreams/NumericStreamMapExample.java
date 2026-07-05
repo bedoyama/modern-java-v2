64c9421f1aa9fd6670c96086a662a04ff9f4c858
Mauricio Bedoya <bedoyama@gmail.com>
2026-07-04 20:58:51 -0500

    misc changes to code - exercises all

--- java-8/src/com/learnJava/numericstreams/NumericStreamMapExample.java
+++ java-8/src/com/learnJava/numericstreams/NumericStreamMapExample.java
@@ -2,48 +2,55 @@ package com.learnJava.numericstreams;
 
 import java.util.List;
 import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.reducing;
 import static java.util.stream.Collectors.toList;
 
 public class NumericStreamMapExample {
 
-
-    public static List<Integer> mapToObj(){
-
-        List<Integer> integerList = IntStream.rangeClosed(1,5)
-                .mapToObj((i)-> {
-                    return new Integer(i);
-                })
+    public static List<Integer> mapToObj() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> i)                    // autoboxing
                 .collect(toList());
-
-        return integerList;
-
     }
 
-    public static double mapToDouble(){
-
-      return   IntStream.rangeClosed(1,5)
-                .mapToDouble((i) ->  i)
-                .sum();
-
+    public static List<String> mapToObjWithTransformation() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToObj(i -> "Number-" + i)        // mapping to String
+                .collect(toList());
     }
 
-    public static long mapToLong(){
-
-        return   IntStream.rangeClosed(1,5)
-                .mapToLong((i) ->  i)
+    public static double mapToDouble() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToDouble(i -> i)                 // primitive double
                 .sum();
+    }
 
+    public static double mapToDoubleWithCalculation() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToDouble(i -> i * i)             // square each number
+                .sum();
+    }
+
+    public static long mapToLong() {
+        return IntStream.rangeClosed(1, 5)
+                .mapToLong(i -> i)
+                .sum();
     }
 
     public static void main(String[] args) {
 
-        System.out.println("mapToObj : " + mapToObj());
+        System.out.println("=== mapToObj() - Integer ===");
+        System.out.println(mapToObj());
 
-        System.out.println("mapToDouble() : " + mapToDouble());
+        System.out.println("=== mapToObj() - String transformation ===");
+        System.out.println(mapToObjWithTransformation());
 
-        System.out.println("mapToLong() : " + mapToLong());
+        System.out.println("=== mapToDouble() - Simple sum ===");
+        System.out.println(mapToDouble());
 
+        System.out.println("=== mapToDouble() - Sum of squares ===");
+        System.out.println(mapToDoubleWithCalculation());
+
+        System.out.println("=== mapToLong() ===");
+        System.out.println(mapToLong());
     }
 }
