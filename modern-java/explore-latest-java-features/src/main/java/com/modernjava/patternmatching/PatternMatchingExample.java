4e54fe5c2a820e4d73fbb4a20ce286b3a9b876d8
Mauricio Bedoya <bedoyama@gmail.com>
2026-07-06 13:51:53 -0500

    minor changes

--- modern-java/explore-latest-java-features/src/main/java/com/modernjava/patternmatching/PatternMatchingExample.java
+++ modern-java/explore-latest-java-features/src/main/java/com/modernjava/patternmatching/PatternMatchingExample.java
@@ -15,5 +15,32 @@ public class PatternMatchingExample {
 
     }
 
+    public String patternWithBindingVariable(Object o) {
+        if (o instanceof Integer i) {
+            return "Integer:" + i;
+        }
+        if (o instanceof String s) {
+            return "String of length:" + s.length();
+        }
+        return "Not a String or Integer with binding variable";
+    }
 
+    public String patternWithGuard(Object o) {
+        if (o instanceof Integer i && i > 10) {
+            return "Integer greater than 10:" + i;
+        }
+        if (o instanceof String s && s.length() > 5) {
+            return "String of length greater than 5:" + s.length();
+        }
+        return "Not a String or Integer with guard";
+    }
+
+    public String patternMatchingWithSwitch(Object o) {
+        return switch (o) {
+            case Integer i -> "Integer:" + i;
+            case String s -> "String of length:" + s.length();
+            case Number n -> "Number:" + n;
+            case null, default -> "Not a String or Integer with switch";
+        };
+    }
 }
--- modern-java/explore-latest-java-features/src/test/java/com/modernjava/patternmatching/PatternMatchingExampleTest.java
+++ modern-java/explore-latest-java-features/src/test/java/com/modernjava/patternmatching/PatternMatchingExampleTest.java
@@ -11,14 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatternMatchingExampleTest {
 
-     PatternMatchingExample patternMatchingExample
-             = new PatternMatchingExample();
+    PatternMatchingExample patternMatchingExample
+            = new PatternMatchingExample();
 
     @ParameterizedTest
     @MethodSource("input")
     void pattern(Object value, String expectedResult) {
 
-        var output =  patternMatchingExample.pattern(value);
+        var output = patternMatchingExample.pattern(value);
         assertEquals(expectedResult, output);
     }
 
@@ -30,4 +30,54 @@ class PatternMatchingExampleTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("inputWithBindingVariable")
+    void patternWithBindingVariable(Object value, String expectedResult) {
+
+        var output = patternMatchingExample.patternWithBindingVariable(value);
+        assertEquals(expectedResult, output);
+    }
+
+    private static Stream<Arguments> inputWithBindingVariable() {
+        return Stream.of(
+                Arguments.of("Dilip", "String of length:5"),
+                Arguments.of(1, "Integer:1"),
+                Arguments.of(null, "Not a String or Integer with binding variable")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputWithGuard")
+    void patternWithGuard(Object value, String expectedResult) {
+
+        var output = patternMatchingExample.patternWithGuard(value);
+        assertEquals(expectedResult, output);
+    }
+
+    private static Stream<Arguments> inputWithGuard() {
+        return Stream.of(
+                Arguments.of("Dilip", "Not a String or Integer with guard"),
+                Arguments.of("Dilip Kumar", "String of length greater than 5:11"),
+                Arguments.of(1, "Not a String or Integer with guard"),
+                Arguments.of(11, "Integer greater than 10:11"),
+                Arguments.of(null, "Not a String or Integer with guard")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputWithMatchingSwitch")
+    void patternMatchingWithSwitch(Object value, String expectedResult) {
+
+        var output = patternMatchingExample.patternMatchingWithSwitch(value);
+        assertEquals(expectedResult, output);
+    }
+
+    private static Stream<Arguments> inputWithMatchingSwitch() {
+        return Stream.of(
+                Arguments.of("Dilip", "String of length:5"),
+                Arguments.of(1, "Integer:1"),
+                Arguments.of(1.0, "Number:1.0"),
+                Arguments.of(null, "Not a String or Integer with switch")
+        );
+    }
 }
