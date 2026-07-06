9d048a5f776c8827e974c1bdddbbff39f47dbcc9
Mauricio Bedoya <bedoyama@gmail.com>
2026-07-06 13:37:35 -0500

    minor changes

--- modern-java/explore-latest-java-features/src/main/java/com/modernjava/sealed/Car.java
+++ modern-java/explore-latest-java-features/src/main/java/com/modernjava/sealed/Car.java
@@ -1,9 +1,15 @@
 package com.modernjava.sealed;
 
-public sealed class Car extends Vehicle permits ElectricCar, GasolineCar {
+public sealed class Car extends Vehicle implements SmartMediaPlayer permits ElectricCar, GasolineCar {
 
     @Override
     public String drive() {
         return "Car";
+    }
+
+    @Override
+    public void connectPhone() {
+        // Implementation for connecting a phone
+        System.out.println("Connecting phone to Car's media player...");
     }
 }
